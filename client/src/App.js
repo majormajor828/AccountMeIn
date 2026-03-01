eee52880f7bcc12f50c8074ff94753174beaeaca
Brandon <bmmajor@pccc.edu>
2026-03-01 10:05:46 -0500

    Firts commit lets gooo

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from "./header"
+import Content from './content';
+
+function App() {
+  return (
+    <div className="bg-gray-900">
+      <Header/>
+      <Content />
+    </div>
+  );
+}
+
+export default App;
